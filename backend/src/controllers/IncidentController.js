ba801cd1ceb4ca18abfa911a280b57c1063a7b7c
Jefferson Barros <jeffersonbds_barros@hotmail.com>
2020-03-28 19:13:47 -0300

    Projeto em andamento

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,7 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request, response){
+        const {title, description, value} = request.body;
+    }
+};
